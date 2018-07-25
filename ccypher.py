58fe3711992cbc00ce60be8e6dd38a3f114f312c
Erik Skogfeldt <EASEAS@users.noreply.github.com>
2018-07-25 08:44:28 -0400

    PEP8 Compliance

--- ccypher.py
+++ ccypher.py
@@ -1,18 +1,18 @@
-
-
+# Erik Skogfeldt
+# 07-25-2018
 alphaRot = int(input('Enter a integer for ceasar cypher: '))
 sentence = str(input('Enter cleartext to be encyphered: '))
 cypherText = ""
 words = []
 
 
-for line in sentence:  
-    for ch in line:	
+for line in sentence:
+    for ch in line:
         if (ord(ch) > 96 and ord(ch) < 123):
-            cypherText = cypherText + chr( ( (ord(ch)+alphaRot - 97) % 26 ) + 97 )
+            cypherText = cypherText + chr(((ord(ch)+alphaRot - 97) % 26) + 97)
         elif (ord(ch) > 64 and ord(ch) < 91):
-            cypherText = cypherText + chr( ( (ord(ch)+alphaRot - 65) % 26) + 65 )
+            cypherText = cypherText + chr(((ord(ch)+alphaRot - 65) % 26) + 65)
         else:
             cypherText = cypherText + ch
-            
-            #print( "character " + ch + " could not be read" )
+
+words = str.split(cypherText)
